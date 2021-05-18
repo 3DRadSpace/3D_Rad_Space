db427fc1e2c1fb364d068a1ed6e42dfbf11bcb63
Athanatos <3dradspace@gmail.com>
2021-05-18 12:51:34 +0300

    Work from high school

--- 3DRadSpace/Lib3DRadSpace_DX/ScriptingFunctionsOldStyle.cs
+++ 3DRadSpace/Lib3DRadSpace_DX/ScriptingFunctionsOldStyle.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using Microsoft.Xna.Framework;
 
 namespace Lib3DRadSpace_DX
 {
@@ -67,10 +69,77 @@ namespace Lib3DRadSpace_DX
         /// 
         /// </summary>
         /// <param name="id"></param>
+        [ObsoleteAttribute("This is not implemented. Using this will always throw a NotImplementedException")]
         public static void iObjectReset(int id)
         {
             //???
             //How the heck are we even going to store the initial object values? Who wants an exta copy of an array?
+            throw new NotImplementedException("iObjectReset is not implemented because there's a need for an extra array containinig the project data. ImPleMeNT iT yOUrSeLF");
+        }
+        public static void iObjectOrientation(int id, out Quaternion q)
+        {
+            q = CurrentProject.GameObjects[id].Rotation;
+        }
+        public static void iObjectOrientationSet(int id,Quaternion q)
+        {
+            CurrentProject.GameObjects[id].Rotation = q;
+        }
+        [ObsoleteAttribute("Resetting objects won't be implemented in 0.1.0a. This will only set the quaternion.")]
+        public static void iObjectOrientationReset(int id,Quaternion q)
+        {
+            CurrentProject.GameObjects[id].Rotation = q;
+        }
+        void iObjectLocation(int id, out Vector3 loc)
+        {
+            loc = CurrentProject.GameObjects[id].Position;
+        }
+        void iObjectLocationSet(int id,Vector3 pos)
+        {
+            CurrentProject.GameObjects[id].Position = pos;
+        }
+        [ObsoleteAttribute("Resetting objects is not implemented. This will only set the object's position.")]
+        void iObjectLocationReset(int id, Vector3 pos)
+        {
+            CurrentProject.GameObjects[id].Position = pos;
+        }
+        [ObsoleteAttribute("Resetting objects is not implemented. This will only set the object's position plus rotation.")]
+        void iObjectPositionReset(int id,Quaternion q,Vector3 pos)
+        {
+            CurrentProject.GameObjects[id].Rotation = q;
+            CurrentProject.GameObjects[id].Position = pos;
+        }
+        void iObjectScaleSet(int id,int scale)
+        {
+            CurrentProject.GameObjects[id].Scale = new Vector3(scale,scale,scale);
+        }
+        int iObjectScaleSet(int id,Vector3 s)
+        {
+            CurrentProject.GameObjects[id].Scale = s;
+        }
+        void iObjectScale(int id, out Vector3 scale)
+        {
+            scale = CurrentProject.GameObjects[id].Scale;
+        }
+        float iObjectKmh(int id)
+        {
+            //TODO: Determine speed for dynamic rigicbodies and FPVCameras. Do the physics interface check. Return 0 if not.
+            return 0;
+        }
+        void iObjectVelocity(int id,out Vector3 vel)
+        {
+            vel = Vector3.Zero;
+        }
+        void iObjectVelocitySet(int id,Vector3 vel)
+        {
+            IPhysicsObject pobj = (IPhysicsObject)CurrentProject.GameObjects[i].ApplyForce(vel.Normalize(),Physics.ForceType.Acceleration,vel.Lenght); //check for comillation errors as this is written inside a browser lel       
+        }
+        void iObjectSpin(int id,out Vector3 speen)
+        {
+            speen = Vector3.Zero;
+        }
+        void iObjectSpinSet(int id,Vector3 spin)
+        { 
+            IPhysicsObject pobj = (IPhysicsObject)CurrentProject.GameObjects[i].ApplyForce(vel.Normalize(),Physics.Spin.Spin,vel.Lenght);
         }
     }
 }
