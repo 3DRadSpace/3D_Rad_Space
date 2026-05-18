8fac03f6c1897f12f38ee61773c09c08cf46af8d
NicusorN5 <NicusorN5@proton.me>
2026-05-19 00:35:38 +0300

    Cone lambert lighting

--- 3DRadSpace/Engine3DRadSpace/Graphics/Primitives/Cone.hpp
+++ 3DRadSpace/Engine3DRadSpace/Graphics/Primitives/Cone.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "IPrimitive.hpp"
+#include "../Rendering/DirectionalLight.hpp"
 
 namespace Engine3DRadSpace::Graphics::Primitives
 {
@@ -11,8 +12,15 @@ namespace Engine3DRadSpace::Graphics::Primitives
 	public:
 		Cone(IGraphicsDevice* device, float radius, float height, unsigned resolution, const Math::Color& color);
 
-		[[nodiscard]] static std::vector<VertexPositionColor> CreateConeVertices(float radius, float height, unsigned resolution, const Math::Color& color);
+		[[nodiscard]] static std::vector<VertexPositionNormalColor> CreateConeVertices(float radius, float height, unsigned resolution, const Math::Color& color);
 		[[nodiscard]] static std::vector<unsigned> CreateConeIndices(unsigned resolution);
+
+		/// <summary>
+		/// Draws the cone with default lighting parameters.
+		/// </summary>
+		void Draw3D() override;
+
+		Rendering::DirectionalLight Light;
 
 		~Cone() override = default;
 	};
