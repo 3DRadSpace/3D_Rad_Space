4aa801a4d9e7ec2b0511e8dcf25cb2506f828836
NicusorN5 <NicusorN5@proton.me>
2026-08-31 00:08:12 +0300

    Fix default objects not showing

--- 3DRadSpace/3DRadSpace_Editor_WindowsDX11/Main.cpp
+++ 3DRadSpace/3DRadSpace_Editor_WindowsDX11/Main.cpp
@@ -96,6 +96,7 @@ int __stdcall WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 	InitializeGDI();
 	InitializeCommonControls();
 	SetWorkingDirectory();
+	Internal::LoadDefaultObjects();
 
 	if (FAILED(CoInitializeEx(nullptr, COINIT::COINIT_MULTITHREADED)))
 		return -1;
